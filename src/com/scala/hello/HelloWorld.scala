a2b652905e178e44fde939ee25af30f2f160896d
wxmimperio <wxmimperio@outlook.com>
2016-03-10 10:46:24 +0800

    Initial commit

---
+++ src/com/scala/hello/HelloWorld.scala
@@ -0,0 +1,17 @@
+package com.scala.hello
+
+/**
+ * Created by wxmimperio on 2016/3/10.
+ */
+//静态方法
+object HelloWorld {
+	//Array数组,[数组元素类型]
+	//Unit空值,即无返回值,可以去掉
+	def main(args: Array[String]): Unit = {
+		println("Hello World")
+		//运行时向main函数传值
+		for(arg <- args) {
+			println(arg)
+		}
+	}
+}
